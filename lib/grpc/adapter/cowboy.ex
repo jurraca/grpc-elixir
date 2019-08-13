30f9ffc5ea058a97caafa16e5b323d969cec421e
Mairbek Khadikov <mairbek@brex.com>
2019-08-13 13:11:38 -0700

    bump number of acceptors

--- lib/grpc/adapter/cowboy.ex
+++ lib/grpc/adapter/cowboy.ex
@@ -7,7 +7,7 @@ defmodule GRPC.Adapter.Cowboy do
   require Logger
   alias GRPC.Adapter.Cowboy.Handler, as: Handler
 
-  @default_num_acceptors 20
+  @default_num_acceptors 256
 
   # Only used in starting a server manually using `GRPC.Server.start(servers)`
   @spec start(atom, GRPC.Server.servers_map(), non_neg_integer, keyword) ::
